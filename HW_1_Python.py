a9c485749c237bb9ddad9f389ff6d7486ebffd68
AlexKantrov <akantrov82@gmail.com>
2022-03-14 13:09:15 +0300

    HW_1_Python

---
+++ HW_1_Python.py
@@ -0,0 +1,38 @@
+# Create variables of different types
+a = 'Hello World'
+b = 5
+c = 2.5
+d = bytes(2)
+e = [10, 20, "ALex", [32, 53, True]]
+f = ((13), ('Vadim'), (1.5))
+j = {"Hello", "Mike"}
+h = frozenset(j)
+i = {"Alex": 32, "Svetlana": 34}
+
+# Print all variables with types
+print(type(a), "= " + a + ";", "\n",
+      type(b), "= " + str(b) + ";", "\n",
+      type(c), "= " + str(c) + ";", "\n",
+      type(d), "= " + str(d) + ";", "\n",
+      type(e), "= " + str(e) + ";", "\n",
+      type(f), "= " + str(f) + ";", "\n",
+      type(j), "= " + str(j) + ";", "\n",
+      type(h), "= " + str(h) + ";", "\n",
+      type(i), "= " + str(i) + "."
+      )
+
+# Create two string variables, concatenate these variables and print the result
+xx = "Intel Core"
+yy = "i9-12900K"
+zz = xx + ' ' + yy
+print("Processor :", zz)
+
+# Print two variables (type: string and integer) using comma
+pp = "Samsung SSD"
+jj = 980
+print("SSD :", pp, jj)
+
+# Print different variables (type: string and integer) using plus
+vv = "Corsair HX"
+cc = 1200
+print("Power supply : " + vv + str(cc))
